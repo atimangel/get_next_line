797a8f2e0728e2b7f426dacfe92118b2be828179
aaa <atimangel@naver.com>
2021-01-05 10:58:09 +0900

    header add functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 14:13:50 by snpark            #+#    #+#             */
-/*   Updated: 2021/01/05 09:56:06 by snpark           ###   ########.fr       */
+/*   Updated: 2021/01/05 10:28:34 by senoi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,9 @@
 int get_next_line(int fd, char **line);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	ft_strjoin(char const *s1, char const *s2);
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+
 #endif
